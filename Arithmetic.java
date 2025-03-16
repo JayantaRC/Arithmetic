036b41c24c6249d5ff08020f4136ccc6dc53a61c
JayantaRC <omjayantaom@gmail.com>
2025-03-16 20:28:22 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,27 +1,26 @@
 
-package com.mycompany.javatutorial;
+package com.mycompany.javatutorial;  //Work In Progress
 
-import java.util.Scanner;
+import java.util.Scanner;  //Work In Progress
 public class Arithmetic {
     public static void main (String[] args){
         
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner (System.in);  //Work In Progress
         
         int price, numberInstallment, installmentmonth;
        
-        System.out.println("Price 1800$");
+        System.out.println("Price 1800$");  //Work In Progress
         price = 1800;
-        
-        double interest = 1.5;
+                                   //Work In Progress
         
         System.out.print("Enter Number Installment : ");
-        numberInstallment = input.nextInt();
+        numberInstallment = input.nextInt(); //Work In Progress
         
-        installmentmonth = price/numberInstallment;
+        installmentmonth = price/numberInstallment;               //Work In Progress
         
-        System.out.println("Your Monthly EMI : "+installmentmonth);
-        System.out.print("Your Monthly Interest : "+installmentmonth*interest);
-        System.out.print("%");
+        System.out.println("Your Monthly EMI : "+installmentmonth); //Work In Progress
+        System.out.print("Your Monthly Interest : "+installmentmonth*interest); //Work In Progress
+        System.out.print("%");  //Work In Progress
     }
     
 }
